462487cba75cc8dd91e0a69cd7105b9a56fa563e
KHIEM luong G <78592170+smarttarded@users.noreply.github.com>
2021-09-16 16:08:40 -0700

    Add files via upload

--- auto-pyng.py
+++ auto-pyng.py
@@ -3,31 +3,108 @@ import subprocess as sp
 import time 
 from datetime import datetime
 
-print("i-comit.github.io \n")
-name = input("Enter the website url: ")
-interval = float(input("Enter interval time in seconds: "))
+import sys
+from PyQt5.QtWidgets import QLabel, QMainWindow,QDesktopWidget, QApplication, QTextEdit, QWidget, QPushButton, QAction, QLineEdit, QMessageBox
+from PyQt5.QtGui import QIcon, QTextBlock
+from PyQt5.QtCore import pyqtSlot
 
-now = datetime.now()
-titletime= datetime.date(now).strftime("%Y%m%d")
+class App(QMainWindow):
 
-starttime=time.time() 
+    def __init__(self):
+        super().__init__()
+        self.title = 'AUTO PYNG 0.0.1'
+        self.left = 10
+        self.top = 10
+        self.width = 700
+        self.height = 300
+        self.initUI()
+    
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.setWindowIcon(QIcon('logo.png'))
+        self.center()
+    
+        # Create url textbox
+        self.urltext = QLineEdit(self)
+        self.urltext.move(20, 20)
+        self.urltext.resize(200,20)
+        # self.textbox = QLabel("ENTER WEBSITE URL HERE", self)
+        # self.textbox.move(240, 20)
+        # self.textbox.resize(200,20)      
 
-with open(f'{name}-PingTest{titletime}.csv', 'w', newline='') as outcsv:
-    writer = csv.writer(outcsv)
-    writer.writerow(["Time", "Packet Sent", "Packet Received", "Packet Loss", "Status"])
+        # Create time interval textbox
+        self.timetext = QLineEdit(self)
+        self.timetext.move(20, 60)
+        self.timetext.resize(200,20)
+        # self.textbox = QLabel("ENTER TIME INTERVAL HERE", self)
+        # self.textbox.move(240, 60)
+        # self.textbox.resize(200,20)   
+        
+        # Create a button in the window
+        self.button = QPushButton('ENTER', self)
+        self.button.move(20,80)
+        
+        # connect button to function on_click
+        self.button.clicked.connect(self.on_click)
+        self.button.show()
 
-while(True): 
-    output = sp.getoutput(f'ping -n 3 {name} | find "Packets"')
-    print (output)
-    packetsent = output[20:21]
-    packetreceived = output[34:35]
+    def center(self):
+            qr = self.frameGeometry()
+            cp = QDesktopWidget().availableGeometry().center()
+            qr.moveCenter(cp)
+            self.move(qr.topLeft())
+
+    
+    # @pyqtSlot()
+    def on_click(self):
+        name = self.urltext.text()
+        interval = self.timetext.text()
+
+        now = datetime.now()
+        titletime= datetime.date(now).strftime("%Y%m%d")
+
+        starttime=time.time() 
+
+        with open(f'{name}-PingTest{titletime}.csv', 'w', newline="") as outcsv:
+            writer = csv.writer(outcsv)
+            writer.writerow(["Time", "Packet Sent", "Packet Received", "Packet Loss", "Status"])
+            
+            output = sp.getoutput(f'ping -n 3 {name} | find "Packets"')
+            print (output)
+            packetsent = output[20:21]
+            packetreceived = output[34:35]
+            packetloss = output[44:45]
+            print(packetloss)
+
+            if(packetsent == packetreceived):
+                status= "UP"
+            elif(packetsent <= (packetsent - 1) > 0):
+                status="ERROR"
+            else:
+                status="DOWN"
+
+            with open(f'{name}-PingTest{titletime}.csv', 'a', newline="") as f:
+                writef = csv.writer(f)
+                t = time.localtime()
+                current_time = time.strftime("%H:%M:%S", t)
+
+                writef.writerow([current_time] + [packetsent] + [packetreceived] + [packetloss] + [status])
+
+                time.sleep(interval - ((time.time() - starttime) % interval))
 
 
-    with open(f'{name}-PingTest{titletime}.csv', 'a') as f:
-        writef = csv.writer(f)
-        t = time.localtime()
-        current_time = time.strftime("%H:%M:%S", t)
+                # QMessageBox.question(self, 'Message - pythonspot.com', "You typed: " + textboxValue, QMessageBox.Ok, QMessageBox.Ok)
+                self.textbox.setText("")
 
-        writef.writerow( [current_time] + [packetsent] + [packetreceived])
 
-        time.sleep(interval - ((time.time() - starttime) % interval))
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    ex.show()
+    sys.exit(app.exec_())
+
+# print("i-comit.github.io \n")
+# name = input("Enter the website url: ")
+# interval = float(input("Enter interval time in seconds: "))
+
